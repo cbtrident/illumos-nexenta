c8d9f46713df6cf3034eb3a35a0a5a71ee6265bc
Rick McNeal <rick.mcneal@nexenta.com>
2016-08-31 08:59:36 -0600

    NEX-6832 fcsm module's debug level default should be 0 Reviewed by: Rob Gittins <rob.gittins@nexenta.com> Reviewed by: Steve Peng <steve.peng@nexenta.com> Reviewed by: Evan Layton <evan.layton@nexenta.com>

--- usr/src/uts/common/io/fibre-channel/ulp/fcsm.c
+++ usr/src/uts/common/io/fibre-channel/ulp/fcsm.c
@@ -21,6 +21,7 @@
 /*
  * Copyright 2009 Sun Microsystems, Inc.  All rights reserved.
  * Use is subject to license terms.
+ * Copyright 2016 Nexenta Systems, Inc. All rights reserved.
  */
 
 /*
@@ -64,8 +65,7 @@ static clock_t		fcsm_offline_ticks;
 
 
 #ifdef DEBUG
-uint32_t		fcsm_debug = (SMDL_TRACE | SMDL_IO |
-    SMDL_ERR | SMDL_INFO);
+uint32_t		fcsm_debug = 0;
 #endif
 
 
