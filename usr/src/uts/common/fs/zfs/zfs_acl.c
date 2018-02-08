6d0b579e22f08294e66cc48d90c3e122c7e35e1a
Gordon Ross <gwr@nexenta.com>
2018-02-08 09:03:33 -0500

    NEX-15035 Allow user ACE in ACL to match SID in token extra SIDs (cleanup) Reviewed by: Yuri Pankov <yuri.pankov@nexenta.com> Reviewed by: Roman Strashkin <roman.strashkin@nexenta.com>

--- usr/src/uts/common/fs/zfs/zfs_acl.c
+++ usr/src/uts/common/fs/zfs/zfs_acl.c
@@ -2088,17 +2088,13 @@ zfs_zaccess_aces_check(znode_t *zp, uint32_t *working_mode,
 	zfsvfs_t	*zfsvfs = zp->z_zfsvfs;
 	zfs_acl_t	*aclp;
 	int		error;
-	uint64_t 	who;		/* ID from the ACE */
+	uint64_t 	who;		/* FUID from the ACE */
 	uint16_t	type, iflags;
 	uint16_t	entry_type;
 	uint32_t	access_mask;
 	uint32_t	deny_mask = 0;
 	zfs_ace_hdr_t	*acep = NULL;
 	boolean_t	checkit;	/* ACE ID matches */
-	uid_t		gowner;
-	uid_t		fowner;
-
-	zfs_fuid_map_ids(zp, cr, &fowner, &gowner);
 
 	mutex_enter(&zp->z_acl_lock);
 
@@ -2131,13 +2127,13 @@ zfs_zaccess_aces_check(znode_t *zp, uint32_t *working_mode,
 
 		switch (entry_type) {
 		case ACE_OWNER:
-			who = fowner;
+			who = zp->z_uid;
 			/*FALLTHROUGH*/
 		case 0:	/* USER Entry */
 			checkit = zfs_user_in_cred(zfsvfs, who, cr);
 			break;
 		case OWNING_GROUP:
-			who = gowner;
+			who = zp->z_gid;
 			/*FALLTHROUGH*/
 		case ACE_IDENTIFIER_GROUP:
 			checkit = zfs_groupmember(zfsvfs, who, cr);
