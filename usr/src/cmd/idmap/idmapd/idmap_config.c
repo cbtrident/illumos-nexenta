bb268557359b0b1e738b8177aadcb856dccac17f
Gordon Ross <gwr@nexenta.com>
2019-04-19 12:07:51 -0400

    NEX-20549 smb AD join broken if no site name Reviewed by: Evan Layton <evan.layton@nexenta.com> Reviewed by: Matt Barden <matt.barden@nexenta.com>

--- usr/src/cmd/idmap/idmapd/idmap_config.c
+++ usr/src/cmd/idmap/idmapd/idmap_config.c
@@ -20,7 +20,7 @@
  */
 /*
  * Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright 2018 Nexenta Systems, Inc.  All rights reserved.
+ * Copyright 2019 Nexenta Systems, Inc.  All rights reserved.
  */
 
 
@@ -1646,10 +1646,11 @@ idmap_cfg_load_smf(idmap_cfg_handles_t *handles, idmap_pg_config_t *pgcfg,
 		    pgcfg->domain_name[0] == '\0') {
 			free(pgcfg->domain_name);
 			pgcfg->domain_name = NULL;
+		} else {
+			pgcfg->domain_name_auto_disc = B_FALSE;
 		}
 		(void) ad_disc_set_DomainName(handles->ad_ctx,
 		    pgcfg->domain_name);
-		pgcfg->domain_name_auto_disc = B_FALSE;
 	}
 
 	rc = get_val_astring(handles, "default_domain",
@@ -1742,16 +1743,30 @@ idmap_cfg_load_smf(idmap_cfg_handles_t *handles, idmap_pg_config_t *pgcfg,
 	if (rc != 0)
 		(*errors)++;
 	else {
+		if (pgcfg->forest_name != NULL &&
+		    pgcfg->forest_name[0] == '\0') {
+			free(pgcfg->forest_name);
+			pgcfg->forest_name = NULL;
+		} else {
+			pgcfg->forest_name_auto_disc = B_FALSE;
+		}
 		(void) ad_disc_set_ForestName(handles->ad_ctx,
 		    pgcfg->forest_name);
-		pgcfg->forest_name_auto_disc = B_FALSE;
 	}
 
 	rc = get_val_astring(handles, "site_name", &pgcfg->site_name);
 	if (rc != 0)
 		(*errors)++;
-	else
+	else {
+		if (pgcfg->site_name != NULL &&
+		    pgcfg->site_name[0] == '\0') {
+			free(pgcfg->site_name);
+			pgcfg->site_name = NULL;
+		} else {
+			pgcfg->site_name_auto_disc = B_FALSE;
+		}
 		(void) ad_disc_set_SiteName(handles->ad_ctx, pgcfg->site_name);
+	}
 
 	rc = get_val_ds(handles, "global_catalog", 3268,
 	    &pgcfg->global_catalog);
